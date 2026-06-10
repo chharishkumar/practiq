6e97e2690e5f1ebfbbea031bbdce066d49fe0db8
chharishkumar <mail2harshac@gmail.com>
2026-06-10 18:20:29 +0530

    Blog page changes

--- src/BlogWritePage.js
+++ src/BlogWritePage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { supabase } from "./supabase";
 import { useMobile } from "./hooks/useMobile";
 import { useEditor, EditorContent } from "@tiptap/react";
